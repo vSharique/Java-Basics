da10ce478620dd4235d56bbe519bfb0e1a9ca539
Sharique Masood <sharique.vicky@gmail.com>
2021-08-17 19:47:23 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -15,7 +15,7 @@ public class Pattern1 {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-		sc.close();
+	sc.close();
         int i = 1;
         while(i <= N) {
             int space = 1;
